7d76ad56e924ac7690ebaf341196dbb5dc1ea0ae
Oleh Kotlinskyi <oleh.kotlinskyi@abz.agency>
2021-10-11 22:32:24 +0300

    feat: add tests for length-of-last-word

--- length-of-last-word/length-of-last-word.test.js
+++ length-of-last-word/length-of-last-word.test.js
@@ -1,9 +1,13 @@
 const lengthOfLastWord = require("./length-of-last-word");
 
 describe("length-of-last-word", () => {
-  it("should ", () => {
-    // console.log(lengthOfLastWord("Hello World"));
-    console.log(lengthOfLastWord(" Hello  World "));
-    // console.log(lengthOfLastWord(" HelloWorld "));
+  it("should return 5 for 'Hello World'", () => {
+    expect(lengthOfLastWord("Hello World")).toBe(5);
+  });
+  it("should return 4 for '   fly me   to   the moon  '", () => {
+    expect(lengthOfLastWord("   fly me   to   the moon  ")).toBe(4);
+  });
+  it("should return 6 for 'luffy is still joyboy'", () => {
+    expect(lengthOfLastWord("luffy is still joyboy")).toBe(6);
   });
 });
